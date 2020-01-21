23a40882acb38536f2fb618894715d7c56f34fd2
Matthijs <matthijsvanderwal99@hotmail.com>
2020-01-21 13:59:10 +0100

    Added: - Comments, minor changes

--- src/MergeData.java
+++ src/MergeData.java
@@ -18,40 +18,40 @@ public class MergeData implements Runnable {
 
     public synchronized void addData(ArrayList<Weatherstation> data){
 
-        if(weatherstations.size() == 0) {
+        if(weatherstations.size() == 0) {                                                                  //If there is no data the first data will be just added without checks
             weatherstations.addAll(data);
-        } else {
-            for (Weatherstation dataSingle : data) {
-                ArrayList<WeatherMeasurement> measurementList = dataSingle.getWeatherMeasurements();
-                for (WeatherMeasurement measurementListItem : measurementList) {
-                    boolean isGefixt = false;
-                    boolean stop = false;
-                    int meaurmentId = measurementListItem.getStn();
-                    for (Weatherstation weatherstation : weatherstations) {
-                        if (weatherstation.stn == meaurmentId) {
-                            ArrayList<WeatherMeasurement> weatherMeasurementArrayList = weatherstation.getWeatherMeasurements();
-                            for (WeatherMeasurement singleWeatherMeasurement :weatherMeasurementArrayList) {
-                                if(singleWeatherMeasurement.getTime() == measurementListItem.getTime()) {
-                                    stop = true;
+    } else {                                                                                               //Otherwise the following check will be executed
+            for (Weatherstation dataSingle : data) {                                                       //For every Weatherstation in the new data:
+                ArrayList<WeatherMeasurement> measurementList = dataSingle.getWeatherMeasurements();       //Make list for all Measurements of the selected Weatherstation
+                for (WeatherMeasurement measurementListItem : measurementList) {                           //Loop trough all the weather measurements:
+                    boolean isGefixt = false;                                                              //Variable to keep track if there is already a weatherstation saved which belongs to the currently receiving measurment
+                    boolean stop = false;                                                                  //Variable to keep track if the currently receiving measurement is a duplicate.
+                    int measurementId = measurementListItem.getStn();                                      //Get id of the station which belongs to the measurment
+                    for (Weatherstation weatherstation : weatherstations) {                                //Loop trough all the weatherstations
+                        if (weatherstation.stn == measurementId) {                                         //Check if weatherstation ID is the same as the Measurement ID
+                            ArrayList<WeatherMeasurement> weatherMeasurementArrayList = weatherstation.getWeatherMeasurements();    //Get the already received measurements from a particular weatherstation
+                            for (WeatherMeasurement singleWeatherMeasurement :weatherMeasurementArrayList) {                        //Loop trough all the measurements of a particular weatherstation
+                                if(singleWeatherMeasurement.getTime() == measurementListItem.getTime()) {                           //Check if the time of arrival is the same as the currently receiving measurement:
+                                    stop = true;                                                                                    //Because this means this is a duplicate of a measurement it not need to be saved.
                                 }
                             }
-                            if(!stop) {
-                                weatherstation.addWeatherMeasurement(measurementListItem);
+                            if(!stop) {                                                                    //Checks if the currently receiving measurement is not a duplicate.
+                                weatherstation.addWeatherMeasurement(measurementListItem);                 //Adds measurement to the correct station.
                             }
-                            isGefixt = true;
+                            isGefixt = true;                                                               //Variable to keep track if there is already a weatherstation saved which belongs to the currently receiving measurment
                         }
                     }
-                    if(isGefixt) {
+                    if(isGefixt) {                                                                         //Checks if all measurements where allocated to the corresponding weatherstation
                         System.out.println("Nice");
-                    } else {
-                        Weatherstation newWeatherStation = new Weatherstation(meaurmentId);
-                        newWeatherStation.addWeatherMeasurement(measurementListItem);
-                        weatherstations.add(newWeatherStation);
+                    } else {                                                                               //Otherwise:
+                        Weatherstation newWeatherStation = new Weatherstation(measurementId);              //A new weatherstation would be made
+                        newWeatherStation.addWeatherMeasurement(measurementListItem);                      //The corresponding measurement will linked to it
+                        weatherstations.add(newWeatherStation);                                            //The new weatherstation will be added to the list of the rest.
                     }
                 }
             }
         }
-        printIt();
+        printIt();          //Print for checking is everything is working.
     }
 
     public void printIt(){
--- src/SAXHandler.java
+++ src/SAXHandler.java
@@ -34,11 +34,10 @@ public class SAXHandler extends DefaultHandler {
     @Override
     public void endDocument() throws SAXException {
         if(timeTillPrintCounter >= timeTillPrint) {
-            json = new Gson().toJson(weatherstations);
-            mergeData.addData(weatherstations);
-            //System.out.println(json);
-            //System.out.println(mergeData.printIt());
-            timeTillPrintCounter = 1;
+            json = new Gson().toJson(weatherstations);  //Makes JSON file from ArrayList (TEST PURPOSES)
+            mergeData.addData(weatherstations);         //Merges all the data into one JSON file
+            //System.out.println(json);                 //Print JSON file from above (TEST PURPOSES)
+            timeTillPrintCounter = 1;                   //Counter for call to print and merge
         }
         timeTillPrintCounter++;
     }
@@ -56,52 +55,52 @@ public class SAXHandler extends DefaultHandler {
     public void endElement(String uri, String localName, String qName) throws SAXException {
         try {
             if (qName.equalsIgnoreCase("MEASUREMENT")) {
-                weatherstation.addWeatherMeasurement(weatherMeasurement);
-            } else if (qName.equalsIgnoreCase("STN")) {
-                int stn = (Integer.valueOf(elementValue));
-                boolean exists = false;
-                Weatherstation existingWeatherStation = null;
-                for (Weatherstation weatherstation:weatherstations) {
-                    if(weatherstation.getStn() == stn) {
-                        exists = true;
-                        existingWeatherStation = weatherstation;
+                weatherstation.addWeatherMeasurement(weatherMeasurement);   //When whole measurement has been read, add it to the corresponding weatherstation.
+            } else if (qName.equalsIgnoreCase("STN")) {        //If it reaches STN do the following tasks:
+                int stn = (Integer.valueOf(elementValue));                  //Make variable of corresponding value
+                boolean exists = false;                                     //Variable to check if STN already exist within a known weatherstation
+                Weatherstation existingWeatherStation = null;               //Variable to hold the already known weatherstation
+                for (Weatherstation weatherstation:weatherstations) {       //Loop trough known weatherstations
+                    if(weatherstation.getStn() == stn) {                    //If a the stn of the measurement is already known:
+                        exists = true;                                      //Weatherstation exists = true
+                        existingWeatherStation = weatherstation;            //The corresponding weatherstation is the one which is found.
                     }
                 }
-                if(!exists) {
-                    weatherstation = new Weatherstation(stn);
-                    weatherstations.add(weatherstation);
-                } else {
-                    weatherstation = existingWeatherStation;
+                if(!exists) {                                               //If the STN is not known already:
+                    weatherstation = new Weatherstation(stn);               //Make a new weatherstation
+                    weatherstations.add(weatherstation);                    //Add it to the list
+                } else {                                                    //If the STN already known by the system:
+                    weatherstation = existingWeatherStation;                //The existing weatherstation is the weatherstation which the measurement belongs to
                 }
-                weatherMeasurement = new WeatherMeasurement();
-                weatherMeasurement.setStn(Integer.valueOf(elementValue));
+                weatherMeasurement = new WeatherMeasurement();              //Make a new weathermeasurement
+                weatherMeasurement.setStn(Integer.valueOf(elementValue));   //Set the Stn variable of the weatherMeasurement to the corresponding one.
             }
              else if (qName.equalsIgnoreCase("DATE")) {
-                weatherMeasurement.setDate(elementValue);
+                weatherMeasurement.setDate(elementValue);                   //Set the Date variable of the weatherMeasurement to the corresponding one.
             } else if (qName.equalsIgnoreCase("TIME")) {
-                weatherMeasurement.setTime(elementValue);
+                weatherMeasurement.setTime(elementValue);                   //Set the Time variable of the weatherMeasurement to the corresponding one.
             } else if (qName.equalsIgnoreCase("TEMP")) {
-                weatherMeasurement.setTemperature(Double.valueOf(elementValue));
+                weatherMeasurement.setTemperature(Double.valueOf(elementValue));        //Set the Temperature variable of the weatherMeasurement to the corresponding one.
             } else if (qName.equalsIgnoreCase("DEWP")) {
-                weatherMeasurement.setDewpoint(Double.valueOf(elementValue));
+                weatherMeasurement.setDewpoint(Double.valueOf(elementValue));           //Set the Dewpoint variable of the weatherMeasurement to the corresponding one.
             } else if (qName.equalsIgnoreCase("STP")) {
-                weatherMeasurement.setAirPressureStationLevel(Double.valueOf(elementValue));
+                weatherMeasurement.setAirPressureStationLevel(Double.valueOf(elementValue));    //Set the AirPressureStationLevel variable of the weatherMeasurement to the corresponding one.
             } else if (qName.equalsIgnoreCase("SLP")) {
-                weatherMeasurement.setAirPressureSeaLevel(Double.valueOf(elementValue));
+                weatherMeasurement.setAirPressureSeaLevel(Double.valueOf(elementValue));        //Set the AirPressureSeaLevel variable of the weatherMeasurement to the corresponding one.
             } else if (qName.equalsIgnoreCase("VISIB")) {
-                weatherMeasurement.setVisibility(Double.valueOf(elementValue));
+                weatherMeasurement.setVisibility(Double.valueOf(elementValue));         //Set the Visibility variable of the weatherMeasurement to the corresponding one.
             } else if (qName.equalsIgnoreCase("WDSP")) {
-                weatherMeasurement.setWindSpeed(Double.valueOf(elementValue));
+                weatherMeasurement.setWindSpeed(Double.valueOf(elementValue));          //Set the Windspeed variable of the weatherMeasurement to the corresponding one.
             } else if (qName.equalsIgnoreCase("PRCP")) {
-                weatherMeasurement.setRainfall(Double.valueOf(elementValue));
+                weatherMeasurement.setRainfall(Double.valueOf(elementValue));           //Set the Rainfall variable of the weatherMeasurement to the corresponding one.
             } else if (qName.equalsIgnoreCase("SNDP")) {
-                weatherMeasurement.setSnowfall(Double.valueOf(elementValue));
+                weatherMeasurement.setSnowfall(Double.valueOf(elementValue));           //Set the Snowfall variable of the weatherMeasurement to the corresponding one.
             } else if (qName.equalsIgnoreCase("FRSHTT")) {
-                weatherMeasurement.setFRSHTT(elementValue);
+                weatherMeasurement.setFRSHTT(elementValue);                             //Set the FRSHTT variable of the weatherMeasurement to the corresponding one.
             } else if (qName.equalsIgnoreCase("CLDC")) {
-                weatherMeasurement.setCloudy(Double.valueOf(elementValue));
+                weatherMeasurement.setCloudy(Double.valueOf(elementValue));             //Set the Cloudy variable of the weatherMeasurement to the corresponding one.
             } else if (qName.equalsIgnoreCase("WNDDIR")) {
-                weatherMeasurement.setWindDirection(Short.valueOf(elementValue));
+                weatherMeasurement.setWindDirection(Short.valueOf(elementValue));       //Set the WindDirection variable of the weatherMeasurement to the corresponding one.
             }
 //            for (WeatherMeasurement weatherMeasurement:
 //                    weatherstation.getWeatherMeasurements()) {
