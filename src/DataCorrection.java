ac7736e08ae5bef6d2153fc0c7fec7775da3bd66
Nicky_de_Scholman <p.nick_scholma@hotmail.com>
2020-01-22 12:13:07 +0100

    Things are getting hot! (or cold) DataCorrection can now also adjust temperature deviations.

--- src/DataCorrection.java
+++ src/DataCorrection.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 /**
  * The datacorrection program takes the data from the weather stations and corrects missing data by extrapolating
  * from the previous 30 values (or the next 30 if the error occurs in the first 30 values). It also corrects
- * temperature values that deviate for more than 25% of the average value
+ * temperature values that deviate for more than 20% of the average value
  *
  * @author Nick Scholma
  * @version 1.0
@@ -31,6 +31,16 @@ public class DataCorrection {
         for (Integer printable : weatherData) {
             System.out.println(printable);
         } //TODO gotta ccheck if the main method can't return it's field to the caller.
+        System.out.println(correctTemperature(makeTestTemps(), 200));
+        System.out.println(correctTemperature(makeTestTemps(), -50));
+    }
+
+    /**
+     * Getter for the weatherdata field.
+     * @return an arraylist containing integers that represent the weatherdata.
+     */
+    public static ArrayList<Integer> getWeatherData() {
+        return weatherData;
     }
 
     /**
@@ -59,16 +69,47 @@ public class DataCorrection {
                     }
                     whereWeAt++; //makes us go to to the item in the weatherdata array
                 }
-                Integer sum = 0;
+                int sum = 0;
                 for (Integer number : previousThirtyValues) {
                     sum += number;
                 }
-                sum = sum / previousThirtyValues.size(); //this sums all values in the previousThirtyValues list and then divides that sum by the number of items, creating an average
+                Integer avg = sum / previousThirtyValues.size(); //this sums all values in the previousThirtyValues list and then divides that sum by the number of items, creating an average
                 previousThirtyValues.clear(); //clears the list for the next null value we encounter in weatherData
-                weatherData.set(index, sum); //sets the value at the index (the null) as the average we just calculated instead
+                weatherData.set(index, avg); //sets the value at the index (the null) as the average we just calculated instead
             }
             index++; //finally, we increase the index by 1 in order to go to the next value in weatherData.
         }
+    }
+
+    /**
+     * Method which checks and corrects the given temperature (in °C) if that value diverges for an amount greater than
+     * 20% of the average value of the given temperature range. This method should only be used on temperature values
+     * @param temps the previous (or next, I don't care) 30 temperature values (as an ArrayList of Integers) to set the average value to.
+     * @param tempToCheck The temperature that you want to check to see if it is a divergent deviant.
+     * @return returns the checked temperature as an Integer, which has been corrected if it was found to be a deviant.
+     */
+    private static Integer correctTemperature(ArrayList<Integer> temps, Integer tempToCheck) {
+        Integer sum = 0;
+        for (int i = 0; i < 30; i++) {
+            sum += temps.get(i);
+        }
+        int avg = sum / temps.size(); //simple calculation to get the average value of the 30 values given
+        double maxTemp = (avg + (0.2 * avg)); //sets the max tolerable temperature
+        double minTemp = avg - (0.2 * avg); //sets the minimal tolerable temperature
+        if (tempToCheck > maxTemp) { //if the checked temperature is greater than the max tolerable temp:
+            int maxTempRound = (int)Math.round(maxTemp); //then we round down the max value
+            Integer maxTempInteger = new Integer(maxTempRound); // and turn it into an Integer, so it fits in the ArrayList
+            tempToCheck = maxTempInteger; //and the temperature that was found to be a deviant, will be corrected to the new Integer value.
+        }
+        else if (tempToCheck < minTemp) { //if the checked temperature is lesser than the minimal tolerable temp:
+            int minTempRound = (int)Math.round(minTemp);
+            Integer minTempInteger = new Integer(minTempRound);
+            tempToCheck = minTempInteger; //same steps as for maxtemp, but applied to mintemp
+        }
+        else { //if the temperature is not a deviant:
+            ; //we do nothing
+        }
+        return tempToCheck; //the checked temperature is returned, possibly corrected
     }
 
     /**
@@ -85,10 +126,14 @@ public class DataCorrection {
     }
 
     /**
-     * getter for the weatherdata field.
-     * @return an arraylist containing integers that represent the weatherdata.
+     * Simple method that generates test values to test the temperature corrector with
+     * @return returns an ArrayList containing Integers 1 trough 30, of which the average value will be 15.
      */
-    public static ArrayList<Integer> getWeatherData() {
-        return weatherData;
-    } //TODO add shit for temperature correction
+    private static ArrayList<Integer> makeTestTemps() {
+        ArrayList<Integer> testTemps = new ArrayList<>();
+        for (Integer i = 1; i < 31; i++) {
+            testTemps.add(i);
+        }
+        return testTemps;
+    }
 }
