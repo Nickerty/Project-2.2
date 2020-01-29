42914fa1974114e7f55a8046a23ed1235c4faf8c
Chris <chrispdejonge@gmail.com>
2020-01-29 13:20:12 +0100

    Added Animated heatmap.html

--- Website/assets/js/functions.js
+++ Website/assets/js/functions.js
@@ -2,7 +2,7 @@ function readTextFile(file, callback) {
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
     rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
+    rawFile.onreadystatechange = function () {
         if (rawFile.readyState === 4 && rawFile.status == "200") {
             callback(rawFile.responseText);
         }
@@ -11,18 +11,24 @@ function readTextFile(file, callback) {
 }
 
 function locationFromStn(fileLocation, stn) {
-    let longlat = new Array();
+
     readTextFile(fileLocation, function (text) {
+        var returnArray = new Array();
         var json = JSON.parse(text)
-        json.forEach(function (allJson) {
-            var columns = allJson["data"];
-            columns.forEach(function (singleRow) {
-                if (singleRow["STN"] == stn) {
-                    longlat["long"] = singleRow["longitude"];
-                    longlat["lat"] = singleRow["latiude"];
-                }
-            });
-        })
+        stn.forEach(function (singlestn) {
+            let longlat = new Array();
+            json.forEach(function (allJson) {
+                var columns = allJson["data"];
+                columns.forEach(function (singleRow) {
+                    if (singleRow["STN"] == singlestn) {
+                        longlat["long"] = singleRow["longitude"];
+                        longlat["lat"] = singleRow["latiude"];
+                        returnArray.push(longlat);
+                        longlat = [];
+                    }
+                });
+            })
+        });
     });
-    return longlat;
+    return returnArray;
 }
