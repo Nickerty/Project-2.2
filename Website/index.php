35d15eeec778eac55b7c9753372edadd6e68e4b2
Chris <chrispdejonge@gmail.com>
2020-02-03 00:50:19 +0100

    ALL THE XML DONE

--- Website/index.php
+++ Website/index.php
@@ -23,6 +23,7 @@ include './assets/js/php/top10CaribbeanSea.php';
     <link href="assets/css/style.css" rel="stylesheet" type="text/css">
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <script src="https://kit.fontawesome.com/a6dd52fde2.js" crossorigin="anonymous"></script>
+
 </head>
 
 <body>
@@ -91,7 +92,60 @@ include './assets/js/php/top10CaribbeanSea.php';
 
     </div>
     <!-- Left Sidebar End -->
+    <script>
+        function download() {
+            $.getJSON("/json/tbl_name.json", function downloadxml(json) {
+                var jsonload = json;
+                var c = json2xml(jsonload);
+                var hiddenElement = document.createElement('a');
+                hiddenElement.href = 'data:text/xml;charset=utf-8,' + encodeURI(c);
+                hiddenElement.target = '_blank';
+                hiddenElement.download = 'test.xml';
+                hiddenElement.click();
+            });
 
+            function json2xml(json) {
+                var a = json;
+                var c = document.createElement("root");
+                var t = function (v) {
+                    return {}.toString.call(v).split(' ')[1].slice(0, -1).toLowerCase();
+                };
+                var f = function (f, c, a, s) {
+                    c.setAttribute("type", t(a));
+                    if (t(a) != "array" && t(a) != "object") {
+                        if (t(a) != "null") {
+                            c.appendChild(document.createTextNode(a));
+                        }
+                    } else {
+                        for (var k in a) {
+                            var v = a[k];
+                            if (k == "__type" && t(a) == "object") {
+                                c.setAttribute("__type", v);
+                            } else {
+                                if (t(v) == "object") {
+                                    var ch = c.appendChild(document.createElementNS(null, s ? "item" : k));
+                                    f(f, ch, v);
+                                } else if (t(v) == "array") {
+                                    var ch = c.appendChild(document.createElementNS(null, s ? "item" : k));
+                                    f(f, ch, v, true);
+                                } else {
+                                    var va = document.createElementNS(null, s ? "item" : k);
+                                    if (t(v) != "null") {
+                                        va.appendChild(document.createTextNode(v));
+                                    }
+                                    var ch = c.appendChild(va);
+                                    ch.setAttribute("type", t(v));
+                                }
+                            }
+                        }
+                    }
+                };
+                f(f, c, a, t(a) == "array");
+                console.log(c);
+                return c.outerHTML;
+            }
+        }
+    </script>
     <!-- ============================================================== -->
     <!-- Start right Content here -->
     <!-- ============================================================== -->
@@ -128,7 +182,7 @@ include './assets/js/php/top10CaribbeanSea.php';
                             <div class="col-sm">
                                 <div class="float-right d-none d-md-block">
                                     <div class="button-items">
-                                        <button class="btn btn-primary" type="button" id="export_button" data-toggle="button"
+                                        <button class="btn btn-primary" type="button" onclick="download()" id="export_button" data-toggle="button"
                                                 style="margin-top: 0;">
                                             <i class="fas"></i> Export Data
                                         </button>
