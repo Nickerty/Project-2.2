0fb5890adb88ff7ebe6c0fe8ab85d206e1d26b48
LarsBuildy <l.bouman@st.hanze.nl>
2020-02-03 12:35:24 +0100

    added coments

--- Website/assets/js/php/loadRain.php
+++ Website/assets/js/php/loadRain.php
@@ -1,23 +1,24 @@
 <?php
 include 'top10CaribbeanSea.php';
-$array_data = []; // ------------------------------------------------------------------------------------------ creates an empty array
-foreach ($decodedCaribbean as $single) {  // ------------------------------------------------------------------ loop trough the decoded json file
-    $stn = $single["stn"]; // --------------------------------------------------------------------------------- creates the stn variable and save the stn's from the json
-    $rain = getRainfall($single["stn"], $decodedTotal); // ---------------------------------------------------- creates the rain variable  and calls the get rainfall function
+$array_data = []; // ----------------------------------------------------------------------------------------- creates an empty array
+foreach ($decodedCaribbean as $single) { // ------------------------------------------------------------------ loop trough the decoded json file
+    $stn = $single["stn"]; // -------------------------------------------------------------------------------- creates the stn variable and save the stn's from the json
+    $rain = getRainfall($single["stn"], $decodedTotal); // --------------------------------------------------- creates the rain variable  and calls the get rainfall function
     if (is_nan($rain)) {
-        continue; // ------------------------------------------------------------------------------------------ checks if  the rain variable is not a number and continues the script
+        continue;  // ---------------------------------------------------------------------------------------- checks if  the rain variable is not a number and continues the script
     }
     if (!in_array(["stn" => $stn, "rain" => $rain], $array_data)) {
-        array_push($array_data, ["stn" => $stn, "rain" => $rain]); // --------------------------------- if the data is not in the already present in the array add the new data
+        array_push($array_data, ["stn" => $stn, "rain" => $rain]); // -------------------------------- if the data is not in the already present in the array add the new data
     }
 }
 function compare_rain($a, $b)
 {
-    return strnatcmp($b['rain'], $a['rain']); // ------------------------------------------------------------- return a natural order of the rain value
+    return strnatcmp($b['rain'], $a['rain']);  // ------------------------------------------------------------- return a natural order of the rain value
 }
 
 usort($array_data, 'compare_rain'); // -------------------------------------------------- sort the array based on compare_rain function
 $array_data = array_slice($array_data, 0, 10); // ---------------------------------------------- slices the array in sets of 10 elements
+if (sizeof($array_data) > 0) {
 if (sizeof($array_data) > 0) {
     foreach ($array_data as $data) {
         echo "                                                <tr>
